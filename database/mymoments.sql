b55707be83983315585c602ee2780807271f808f
Acacio de Lima <limadeacacio@gmail.com>
2022-10-18 16:40:45 -0300

    Update mymoments.sql

--- database/mymoments.sql
+++ database/mymoments.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `users` (
+﻿CREATE TABLE `users` (
   `id` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
@@ -12,22 +12,19 @@ CREATE TABLE `moments` (
   `moment_id` varchar(255) NOT NULL,
   `moment_label` varchar(255) NOT NULL,
   `moment_description` varchar(255) DEFAULT NULL,
-  `moment_attachments` text DEFAULT NULL,
-  `user_id` varchar(255) NOT NULL,
+  `id_attachment` varchar(255) NOT NULL,
+  `id_user` varchar(255) NOT NULL,
   `update_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`moment_id`),
-  KEY `moments_user_id_foreign` (`user_id`),
-  CONSTRAINT `moments_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+  KEY `moments_id_user_foreign` (`id_user`),
+  CONSTRAINT `moments_id_user_foreign` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `attachments` (
   `attachment_id` varchar(255) NOT NULL,
-  `attachment_file` varchar(255) NOT NULL,
-  `user_id` varchar(255) NOT NULL,
+  `attachments` text NOT NULL,
   `update_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`attachment_id`),
-  KEY `attachments_user_id_foreign` (`user_id`),
-  CONSTRAINT `attachments_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+  PRIMARY KEY (`attachment_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
